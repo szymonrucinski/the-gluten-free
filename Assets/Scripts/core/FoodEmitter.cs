123c54b60c146f09325c385ee242b5b6cab9b128
mka4975 <45996544+mka4975@users.noreply.github.com>
2020-04-03 12:12:36 +0200

    main scene; shop environment; camera movement

---
+++ Assets/Scripts/core/FoodEmitter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodEmitter : MonoBehaviour
+{
+    public GameObject[] Food;
+    public float SpawnIntervall;        // Spawn intervall
+    public float startDelay = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", startDelay, SpawnIntervall);
+    }
+
+    void Update()
+    {
+        //Spawn();
+    }
+
+    void Spawn()
+    {
+        GameObject emittedFood = Instantiate(Food[Random.Range(0, Food.Length)],new Vector3(0.8239996f, 1.175f,-3.39f),transform.rotation);
+    }
+
+
+
+}
---
+++ Assets/Scripts/core/ProductMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductMovement : MonoBehaviour
+{
+    public float Speed = 0.01f;
+    private GameObject Food;
+
+    private Vector3 Move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Move = new Vector3(0, 0, Speed);
+        Food = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z <= 4)
+        {
+            transform.position += Move;
+        }
+        else {
+            Destroy(Food); 
+        }
+    }
+}
