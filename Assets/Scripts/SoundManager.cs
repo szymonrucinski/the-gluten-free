2bd90afe723a35d0593667d778d126c8362726f3
Janik Mayr <jan.222.ik@gmail.com>
2020-04-16 13:24:16 +0200

    Added SoundManager.cs, Main Theme and Volume Controls

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    
+    public Sound[] sounds;
+    
+    [Range(0f, 1f)]
+    public float baseVolume = .75f;
+
+    public Slider[] volumeModifiers;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            //DontDestroyOnLoad(gameObject);
+            foreach (var s in sounds)
+            {
+                s.source = gameObject.AddComponent<AudioSource>();
+                s.source.clip = s.clip;
+                s.source.loop = s.loop;
+                s.source.volume = baseVolume;
+            }
+        }
+
+        foreach (var slider in volumeModifiers)
+        {
+            slider.value = baseVolume;
+        }
+    }
+
+    private void Start()
+    {
+        Play("theme");
+    }
+
+    public void Play(string sound)
+    {
+        var s = Array.Find(sounds, item => item.name == sound);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+    
+    // Used in UI
+    // ReSharper disable once UnusedMember.Global
+    public void setVolume(float volume)
+    {
+        if (Math.Abs(volume - baseVolume) < 0.001f) return;
+        
+        var vol = Math.Max(0f, Math.Min(volume, 1f));
+        // Update Volume
+        foreach (var sound in sounds)
+        {
+            baseVolume = vol;
+            sound.source.volume = vol;
+        }
+        // Update Sliders
+        foreach (var slider in volumeModifiers)
+        {
+            slider.value = baseVolume;
+        }
+    }
+}
