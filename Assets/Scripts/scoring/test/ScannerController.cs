2a14754b0cd6b628b77abe880024d797e209b58c
Emanuel Beer <emanuel.beer@students.fhv.at>
2020-04-21 10:10:37 +0200

    added scanner to scene, added gameenvironment gameobject

--- Assets/Scripts/scoring/test/ScannerController.cs
+++ Assets/Scripts/scoring/test/ScannerController.cs
@@ -7,19 +7,23 @@ public class ScannerController : MonoBehaviour
     {
         if (GameController.Instance.currentGameState == GameState.InGame)
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 2000, Color.white);
 
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit HitInfo))
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                if (HitInfo.collider.CompareTag("good"))
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 2000, Color.white);
+
+                if (Physics.Raycast(transform.position, transform.forward, out RaycastHit HitInfo))
                 {
-                    ScoreController.Instance.scoreAction(true, false, HitInfo.transform.position);
-                    Destroy(HitInfo.collider.gameObject);
-                }
-                else if (HitInfo.collider.CompareTag("bad"))
-                {
-                    ScoreController.Instance.scoreAction(false, false, HitInfo.transform.position);
-                    Destroy(HitInfo.collider.gameObject);
+                    if (HitInfo.collider.CompareTag("good"))
+                    {
+                        ScoreController.Instance.scoreAction(true, false, HitInfo.transform.position);
+                        Destroy(HitInfo.collider.gameObject);
+                    }
+                    else if (HitInfo.collider.CompareTag("bad"))
+                    {
+                        ScoreController.Instance.scoreAction(false, false, HitInfo.transform.position);
+                        Destroy(HitInfo.collider.gameObject);
+                    }
                 }
             }
         }
