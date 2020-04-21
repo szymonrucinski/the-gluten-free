2c9b3e133d4c857d74c603bf8fb49da644caa3b7
Emanuel Beer <emanuel.beer@students.fhv.at>
2020-04-21 16:33:57 +0200

    Update ScannerController.cs

--- Assets/Scripts/scoring/test/ScannerController.cs
+++ Assets/Scripts/scoring/test/ScannerController.cs
@@ -1,7 +1,19 @@
 ﻿using UnityEngine;
 
 public class ScannerController : MonoBehaviour
-{ 
+{
+
+    public LineRenderer laserLine;
+
+    public float lineWidth = 0.05f;
+    public float length = 1000.0f;
+
+    void Start()
+    {
+        laserLine.startWidth = lineWidth;
+        laserLine.endWidth = lineWidth;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -10,21 +22,33 @@ public class ScannerController : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 2000, Color.white);
+                var pos = transform.position;
+                laserLine.enabled = true;
+                laserLine.SetPosition(0,pos);
+                laserLine.SetPosition(1, transform.TransformDirection(Vector3.forward) * length);
 
-                if (Physics.Raycast(transform.position, transform.forward, out RaycastHit HitInfo))
+                Debug.DrawRay(pos, transform.TransformDirection(Vector3.forward) * 2000, Color.white);
+
+
+                if (Physics.Raycast(pos, transform.forward, out RaycastHit HitInfo))
                 {
                     if (HitInfo.collider.CompareTag("good"))
                     {
-                        ScoreController.Instance.scoreAction(true, false, HitInfo.transform.position);
+                        ScoreController.Instance.scoreAction(true, false, HitInfo.point);
                         Destroy(HitInfo.collider.gameObject);
                     }
                     else if (HitInfo.collider.CompareTag("bad"))
                     {
-                        ScoreController.Instance.scoreAction(false, false, HitInfo.transform.position);
+                        ScoreController.Instance.scoreAction(false, false, HitInfo.point);
                         Destroy(HitInfo.collider.gameObject);
                     }
                 }
+                
+
+            }
+            else
+            {
+                laserLine.enabled = false;
             }
         }
     }
