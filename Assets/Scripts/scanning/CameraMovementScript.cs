2f11df82fe439921f8a636d7551b1c84748a78b1
Maciek Sputowski <maciejsputowski@onet.pl>
2020-05-17 23:44:04 +0200

    Movement fix

--- Assets/Scripts/scanning/CameraMovementScript.cs
+++ Assets/Scripts/scanning/CameraMovementScript.cs
@@ -26,12 +26,12 @@ public class CameraMovementScript : MonoBehaviour
         scanner = GameObject.Find("Scanner-Eward");
         initialBagPosition = new GameObject();
         initialBagPosition.transform.position = bag.transform.position;
-        initialBagPosition.transform.parent = transform; 
+        initialBagPosition.transform.parent = Camera.main.transform; 
         initialScannerPosition = new GameObject();
         initialScannerPosition.transform.position = scanner.transform.position;
-        initialScannerPosition.transform.parent = transform;
-        lastX = transform.position.x;
-        lastZ = transform.position.z;
+        initialScannerPosition.transform.parent = Camera.main.transform;
+        lastX = Camera.main.transform.position.x;
+        lastZ = Camera.main.transform.position.z;
 
         device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
     }
@@ -41,46 +41,46 @@ public class CameraMovementScript : MonoBehaviour
     {
         moving = false;
         device.TryGetFeatureValue(CommonUsages.primary2DAxis, out touchpadState);
-        if (touchpadState.y < 0.0f)
+        if (touchpadState.y < 0.0f || Input.GetKey(KeyCode.DownArrow))
         {
             moving = true;
             if (animationStart == -1.0f) animationStart = Time.time;
-            transform.position += new Vector3(transform.forward.x, 0.0f, transform.forward.z) * -moveSpeed;
+            transform.position += new Vector3(Camera.main.transform.forward.x, 0.0f, Camera.main.transform.forward.z) * -moveSpeed;
             shakeCamera();
         }
-        if (touchpadState.y > 0.0f)
+        if (touchpadState.y > 0.0f || Input.GetKey(KeyCode.UpArrow))
         {
             moving = true;
             if (animationStart == -1.0f) animationStart = Time.time;
-            transform.position += new Vector3(transform.forward.x, 0.0f, transform.forward.z) * moveSpeed;
+            transform.position += new Vector3(Camera.main.transform.forward.x, 0.0f, Camera.main.transform.forward.z) * moveSpeed;
             shakeCamera();
         }
-        if (touchpadState.x < 0.0f)
+        if (touchpadState.x < 0.0f || Input.GetKey(KeyCode.LeftArrow))
         {
             moving = true;
             if (animationStart == -1.0f) animationStart = Time.time;
-            transform.position += new Vector3(transform.right.x, 0.0f, transform.right.z) * -moveSpeed;
+            transform.position += new Vector3(Camera.main.transform.right.x, 0.0f, Camera.main.transform.right.z) * -moveSpeed;
             shakeCamera();
         }
-        if (touchpadState.x > 0.0f)
+        if (touchpadState.x > 0.0f || Input.GetKey(KeyCode.RightArrow))
         {
             if (animationStart == -1.0f) animationStart = Time.time;
             moving = true;
-            transform.position += new Vector3(transform.right.x, 0.0f, transform.right.z) * moveSpeed;
+            transform.position += new Vector3(Camera.main.transform.right.x, 0.0f, Camera.main.transform.right.z) * moveSpeed;
             shakeCamera();
         }
         if (!moving)
         {
             animationStart = -1.0f;
-            if (bag.transform.position != initialBagPosition.transform.position && (lastX != transform.position.x || lastZ != transform.position.z))  
+            if (bag.transform.position != initialBagPosition.transform.position && (lastX != Camera.main.transform.position.x || lastZ != Camera.main.transform.position.z))  
             {
                 bag.transform.position = Vector3.MoveTowards(bag.transform.position, new Vector3(initialBagPosition.transform.position.x, initialBagPosition.transform.position.y, initialBagPosition.transform.position.z), 1.0f * Time.deltaTime);
                 scanner.transform.position = Vector3.MoveTowards(scanner.transform.position, new Vector3(initialScannerPosition.transform.position.x, initialScannerPosition.transform.position.y, initialScannerPosition.transform.position.z), 1.0f * Time.deltaTime);
             }
             else
             {
-                lastX = transform.position.x;
-                lastZ = transform.position.z;
+                lastX = Camera.main.transform.position.x;
+                lastZ = Camera.main.transform.position.z;
             }   
         }
     }
--- Assets/Scripts/scanning/ScanFoodEmitter.cs
+++ Assets/Scripts/scanning/ScanFoodEmitter.cs
@@ -21,6 +21,7 @@ public class ScanFoodEmitter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("Spawn", startDelay, SpawnIntervall);
     }
 
     public void StopSpawning()
