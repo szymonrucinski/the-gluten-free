5d3a2fc25a175612838504ac05de86f18db1325e
Maciek Sputowski <maciejsputowski@onet.pl>
2020-05-17 18:52:07 +0200

    Start game, spawning objects

--- Assets/Scripts/scanning/ScannerVRController.cs
+++ Assets/Scripts/scanning/ScannerVRController.cs
@@ -7,15 +7,33 @@ public class ScannerVRController : MonoBehaviour
 {
     private InputDevice device;
     private Quaternion deviceRotation;
+    private GameController gameController;
+    private ScanFoodEmitter scanFoodEmitter;
+    private bool click;
     // Start is called before the first frame update
     void Start()
     {
+        gameController = GameController.Instance;
+        scanFoodEmitter = ScanFoodEmitter.Instance;
         device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
     }
 
     // Update is called once per frame
     void Update()
     {
+        device.TryGetFeatureValue(CommonUsages.triggerButton, out click);
+        if (gameController.currentGameState == GameState.SHOW_SHOPPING_LIST)
+        {
+            if (click)
+            {
+                gameController.StartGame();
+                scanFoodEmitter.StartSpawning();
+            }
+            else
+            {
+                scanFoodEmitter.StopSpawning();
+            }
+        }
         if (device.TryGetFeatureValue(CommonUsages.deviceRotation, out deviceRotation)) transform.localRotation = deviceRotation;
     }
 }
