ec7ad551ae42960c9250291c5152b069a70ea577
Maciek Sputowski <maciejsputowski@onet.pl>
2020-05-17 14:09:22 +0200

    VRController fix

--- Assets/Scripts/scanning/ScannerVRController.cs
+++ Assets/Scripts/scanning/ScannerVRController.cs
@@ -2,34 +2,21 @@
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class ScannerVRController : MonoBehaviour
 {
-    List<UnityEngine.XR.InputDevice> gameControllers = new List<UnityEngine.XR.InputDevice>();
-    UnityEngine.XR.InputDevice device;
+    InputDevice device;
     Quaternion deviceRotation;
     // Start is called before the first frame update
     void Start()
     {
-        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.GameController, gameControllers);
-        if (gameControllers.Count == 1)
-        {
-            device = gameControllers[0];
-            Debug.Log("It's alive");
-        }
-        else if (gameControllers.Count > 1)
-        {
-            Debug.Log("Found more than one left hand!");
-        }
-        else
-        {
-            Debug.Log("0");
-        }
+        device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceRotation, out deviceRotation)) transform.rotation = deviceRotation;
+        if(device.TryGetFeatureValue(CommonUsages.deviceRotation, out deviceRotation)) transform.localRotation = deviceRotation;
     }
 }
